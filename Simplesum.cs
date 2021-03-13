24a44c9ee47b528f7a7e9dc8308d90c8b9e4fe69
FPG02 <00155220@uca.edu.sv>
2021-03-12 21:45:52 -0600

    Modify: From subtracion to multiplication

--- Simplesum.cs
+++ Simplesum.cs
@@ -9,19 +9,19 @@ namespace Example1
 
             int num1 = 0, num2= 0;
 
-            Console.WriteLine("Enter a value for Num1: ");
+            Console.WriteLine("Enter a integer value for Num1: ");
             num1 = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter a value for Num2: ");
+            Console.WriteLine("Enter a integer value for Num2: ");
             num2 = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Adding {num1} + {num2} equals {AddNumbers(num1, num2)} ");
+            Console.WriteLine($"Adding {num1} * {num2} equals {AddNumbers(num1, num2)} ");
         }
 
         public static int AddNumbers(int num1, int num2)
         {
 
-            return num1 + num2;
+            return num1 * num2;
         }
     }
 }
